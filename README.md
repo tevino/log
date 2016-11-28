613d0b2628b88e6135663ceb829df3924439a59d
Tevin Zhang <mail2tevin@gmail.com>
2016-11-28 20:55:53 +0800

    Add benchmark

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ A lightweight, leveled logging package for Golang.
 ## Features
 - Leveled logging and filtering
 - Colored output
+
+
+## Benchmark
+```
+BenchmarkLog15-4              300000          4391 ns/op        1202 B/op         16 allocs/op
+BenchmarkLogrus-4             500000          3444 ns/op        1715 B/op         24 allocs/op
+BenchmarkLoggo-4              500000          3275 ns/op         344 B/op         11 allocs/op
+BenchmarkGoLogging-4         1000000          1136 ns/op         601 B/op         10 allocs/op
+BenchmarkThisPackage-4       3000000           731 ns/op         214 B/op          2 allocs/op
+BenchmarkStdLog-4            2000000           571 ns/op         174 B/op          2 allocs/op
+```
---
+++ bench_test.go
@@ -0,0 +1,97 @@
+package log
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/inconshreveable/log15"
+	"github.com/juju/loggo"
+	gologging "github.com/op/go-logging"
+)
+
+var hostname string
+
+func init() {
+	var err error
+	hostname, err = os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func BenchmarkLog15(b *testing.B) {
+	var buf bytes.Buffer
+	var logger = log15.New("test", "test")
+	logger.SetHandler(log15.StreamHandler(&buf, log15.LogfmtFormat()))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Info("This is a test: ", hostname, nil)
+	}
+}
+
+func BenchmarkLogrus(b *testing.B) {
+	var buf bytes.Buffer
+	logrus.SetOutput(&buf)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logrus.WithFields(logrus.Fields{
+			"hostname": hostname,
+		}).Info("This is a test: ")
+	}
+}
+
+func BenchmarkLoggo(b *testing.B) {
+	var buf bytes.Buffer
+	var logger = loggo.GetLogger("")
+	logger.SetLogLevel(loggo.INFO)
+	if _, err := loggo.RemoveWriter("default"); err != nil {
+		panic(err)
+	}
+	var writer = loggo.NewColorWriter(&buf)
+	if err := loggo.RegisterWriter("default", writer); err != nil {
+		panic(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Infof("This is a test: %s", hostname)
+	}
+}
+
+func BenchmarkGoLogging(b *testing.B) {
+	var buf bytes.Buffer
+	var logger = gologging.MustGetLogger("test")
+	var backend = gologging.NewLogBackend(&buf, "test", 0)
+	leveledBackend := gologging.AddModuleLevel(backend)
+	logger.SetBackend(leveledBackend)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Infof("This is a test: %s", hostname)
+	}
+}
+
+func BenchmarkThisPackage(b *testing.B) {
+	var buf bytes.Buffer
+	var logger = NewLeveledLogger(&buf, LstdFlags)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Printf("This is a test: %s", hostname)
+	}
+}
+
+func BenchmarkStdLog(b *testing.B) {
+	var buf bytes.Buffer
+	var logger = log.New(&buf, "test", log.LstdFlags)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		logger.Printf("This is a test: %s", hostname)
+	}
+}
